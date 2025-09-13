236931d5e86561b16eeac90060c1577ff8603416
Net-Gainz <netgainzofficial@gmail.com>
2025-09-13 14:57:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 const communityConfig = {
   ad1: "JAGOfXsG6PG07hwDLm7N11", // WhatsApp ID for Ad 1
   ad2: "ETgbGKv20PE9S25FKlmNyD", // WhatsApp ID for Ad 2
-  ad3: "D6YN8ODKy0b5WNqE0wo4lE",  // WhatsApp ID for Ad 3
+  ad3: "ByI8WKplQErFKeLxWsUnzl",  // WhatsApp ID for Ad 3
   default: "JAGOfXsG6PG07hwDLm7N11" // fallback ID
 };
